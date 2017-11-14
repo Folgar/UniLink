14f675c2a2278c648501cfb9f2b70c6849ef2cdf
Unknown <lorane.lebrun@gmail.com>
2017-11-14 11:32:13 -0500

    Connexion via Facebook fonctionnel

--- app/src/main/java/com/example/uqac/unilink/MainActivity.java
+++ app/src/main/java/com/example/uqac/unilink/MainActivity.java
@@ -11,13 +11,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.FacebookCallback;
+import com.facebook.AccessToken;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     //defining view objects
     private EditText editTextEmail;
@@ -27,6 +40,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView textViewSignin;
 
     private ProgressDialog progressDialog;
+
+    private LoginButton buttonFacebook;
+    private CallbackManager callbackManager;
 
 
     //defining firebaseauth object
@@ -39,6 +55,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //initializing firebase auth object
         firebaseAuth = FirebaseAuth.getInstance();
+
+        // Initialize Facebook SDK
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         //if getCurrentUser does not returns null
         if(firebaseAuth.getCurrentUser() != null){
@@ -56,12 +75,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewSignin = (TextView) findViewById(R.id.textViewSignin);
 
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
+        buttonFacebook = (LoginButton) findViewById(R.id.buttonFacebook);
 
         progressDialog = new ProgressDialog(this);
 
         //attaching listener to button
         buttonSignup.setOnClickListener(this);
         textViewSignin.setOnClickListener(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        buttonFacebook.setReadPermissions("email", "public_profile");
+        buttonFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                //Log.i(TAG,"Hello"+loginResult.getAccessToken().getToken());
+                //Toast.makeText(LoginActivity.this, "Token:"+loginResult.getAccessToken(), Toast.LENGTH_SHORT).show();
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Erreur d'authentication",
+                                    Toast.LENGTH_SHORT).show();
+                            LoginManager.getInstance().logOut();
+                        }
+                    }
+                });
     }
 
     private void registerUser(){
