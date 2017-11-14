a6753531eb7f5a02d8431a375bc2bcc1cb9011a3
Alexis Leniau <alexis.leniau@free.fr>
2017-11-14 14:30:50 -0500

    ajout boutton

--- app/src/main/java/com/example/uqac/unilink/table_ouverte.java
+++ app/src/main/java/com/example/uqac/unilink/table_ouverte.java
@@ -5,21 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Button;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.GregorianCalendar;
+
 
 public class table_ouverte extends AppCompatActivity implements DatePickerFragment.DateDialogListener,TimePickerFragment.TimeDialogListener{
 
     private static final String DIALOG_DATE = "table_ouverte.DateDialog";
     private EditText datePickerAlertDialog;
     private EditText timePickerAlertDialog;
+    private EditText nombre;
+    private EditText lieu;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_ouverte);
         datePickerAlertDialog = (EditText)findViewById(R.id.alert_dialog_date_picker);
         timePickerAlertDialog = (EditText)findViewById(R.id.alert_dialog_time_picker);
+        nombre = (EditText)findViewById(R.id.nombre_participants);
+        lieu = (EditText)findViewById(R.id.lieu);
+
+        button = (Button)findViewById(R.id.validate);
         datePickerAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +37,14 @@ public class table_ouverte extends AppCompatActivity implements DatePickerFragme
             }
         });
 
+        timePickerAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TimePickerFragment dialog = new TimePickerFragment();
+                dialog.show(getSupportFragmentManager(),"TimePickerFragment");
+            }
+        });
         timePickerAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
